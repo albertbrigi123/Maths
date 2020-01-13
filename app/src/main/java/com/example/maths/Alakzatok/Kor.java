959db0d4e3641ef8983a30b1f26abc5c49c50b24
albertbrigi123 <albertbrigi95@gmail.com>
2020-01-13 15:37:36 +0200

    Negyzet

---
+++ app/src/main/java/com/example/maths/Alakzatok/Kor.java
@@ -0,0 +1,16 @@
+package com.example.maths.Alakzatok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.maths.R;
+
+public class Kor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kor);
+    }
+}
